b1d4d6c61887f103cbc114bdcc2cb8d6c3e4dc8c
Cameron Durham <camdurha@amazon.com>
2023-02-01 21:00:28 +0000

    Add ESLint

--- lua/cam/remap.lua
+++ lua/cam/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+---   pattern = { "*" },
+---   command = [[%s/\s\+$//e]],
+--- })
